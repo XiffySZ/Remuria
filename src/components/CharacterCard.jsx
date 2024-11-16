2b566886e513b89b073dc4de61b9eda209e61b15
rounak-codes <rounakbag24@gmail.com>
2024-11-16 17:09:25 +0530

    0.92

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 
-const CharacterCard = ({ image, overlayImage, tags }) => {
+const CharacterCard = ({ image, tags }) => {
   return (
     <div className="character-card">
       {/* Character Image */}
       <img src={image} alt="Character" className="character-image" />
-
-      {/* Overlay Image */}
-      {overlayImage && (
-        <img
-          src={overlayImage}
-          alt="Overlay"
-          className="overlay-image"
-        />
-      )}
 
       {/* Tags or other info */}
       <div className="character-tags">
